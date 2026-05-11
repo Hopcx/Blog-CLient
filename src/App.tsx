e538a223138e01bafc061b7dc202267c05bede2b
bohoplatao <cuhopyi2004@gmail.com>
2026-05-11 21:45:29 +0700

    create CreatePost and ProtectedRoute

--- src/App.tsx
+++ src/App.tsx
@@ -3,16 +3,18 @@ import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
 import { Provider } from 'react-redux'
 import { store } from './store'
 import MainLayout from './components/layout/MainLayout'
+import ProtectedRoute from './components/common/ProtectedRoute'
 import Home from './pages/Home'
 import PostDetail from './pages/PostDetail'
 import Login from './pages/Login'
 import Register from './pages/Register'
+import CreatePost from './pages/CreatePost'
 
 const queryClient = new QueryClient({
   defaultOptions: {
     queries: {
       retry: 1,
-      staleTime: 1000 * 60 * 5, // 5 phút
+      staleTime: 1000 * 60 * 5,
     },
   },
 })
@@ -24,10 +26,16 @@ function App() {
         <BrowserRouter>
           <Routes>
             <Route element={<MainLayout />}>
+              {/* Public routes */}
               <Route path="/" element={<Home />} />
               <Route path="/posts/:slug" element={<PostDetail />} />
               <Route path="/login" element={<Login />} />
               <Route path="/register" element={<Register />} />
+
+              {/* Protected routes */}
+              <Route element={<ProtectedRoute />}>
+                <Route path="/posts/create" element={<CreatePost />} />
+              </Route>
             </Route>
           </Routes>
         </BrowserRouter>
